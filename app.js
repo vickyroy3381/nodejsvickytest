b83ed7d8a93d2b23a1080abb718434048ffd69e0
vickyroy3381 <128716321+vickyroy3381@users.noreply.github.com>
2023-10-27 18:34:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const MemoryStore = require('memorystore')(session);
+//const MemoryStore = require('memorystore')(session);
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const path = require ('path');
@@ -35,14 +35,7 @@ mongoose.connect('mongodb+srv://vickyjsauth:PZxeE0vvWdbZn9fw@mymongodb.mecwaj1.m
 
 // Express middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    cookie: { maxAge: 86400000 },
-    store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
-    }),
-    resave: false,
-    secret: 'keyboard cat'
-}));
+app.use(session({ secret: 'secret-key', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
